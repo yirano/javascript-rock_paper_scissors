36742854e38069793d86c8943da8f5829f624133
yirano <tiffany.arellano@outlook.com>
2019-10-15 21:37:48 -0400

    Remove innerHTML to insert images - this is not controlled by adding and updating the class of images

--- src/script.js
+++ src/script.js
@@ -1,16 +1,17 @@
 let buttons = document.querySelectorAll('button');
-let compScreen = document.querySelector('#computer-screen span');
-let playerScreen = document.querySelector('#player-screen span');
+let compScreen = document.querySelector('#computer-screen p');
+let playerScreen = document.querySelector('#player-screen p');
 let compPts = document.getElementById('compPts');
 let playerPts = document.getElementById('playerPts');
 let announce = document.querySelector('#announce h2');
-announce.textContent = "HI"
 let compPt = 0;
 let playerPt = 0;
+let cOutput, pOutput;
 
 for (const button of buttons) {
 
   button.addEventListener("click", function (e) {
+
       let computerPlay = function () {
         let random = ['Rock', 'Paper', 'Scissor']
         return random[Math.floor(Math.random() * 3)].toLowerCase();;
@@ -21,6 +22,7 @@ for (const button of buttons) {
       }
 
       let play = function (computerSelection = computerPlay(), playerSelection = playerPlay()) {
+
         if (computerSelection == 'rock') {
           if (playerSelection == 'scissor') {
             compPt += 1;
@@ -55,45 +57,14 @@ for (const button of buttons) {
         display(computerSelection, playerSelection);
       }
 
-      let sanitizeHTML = function (str) {
-
-      }
-
       let display = function (computerSelection, playerSelection) {
-        let s = "<img src='../imgs/scissor.svg' style='width: 60px;' />"
-        // let s = "'../imgs/scissor.scg' style='width: 60px; '"
-        let r = "<img src='../imgs/rock.svg' style='width: 60px;'/>";
-        let p = "<img src='../imgs/paper.svg' style='width: 60px;' />"
-        let cOutput, pOutput;
-        switch (computerSelection) {
-          case 'scissor':
-            cOutput = s;
-            break;
-          case 'rock':
-            cOutput = r;
-            break;
-          case 'paper':
-            cOutput = p;
-            break;
-        }
-
-        switch (playerSelection) {
-          case 'scissor':
-            pOutput = s;
-            break;
-          case 'rock':
-            pOutput = r;
-            break;
-          case 'paper':
-            pOutput = p;
-            break;
-        }
-
-        compScreen.innerHTML = cOutput;
-        playerScreen.innerHTML = pOutput;
+        playerScreen.classList = playerSelection;
+        compScreen.classList = computerSelection;
       }
+
 
       play();
+
     },
     false)
 
